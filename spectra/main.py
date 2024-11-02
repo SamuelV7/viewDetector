691e11c0f2c24589bc05d07a8dc89a493b15a46d
Samuel Varghese <gamingsam77@gmail.com>
2024-11-02 00:32:10 +0000

    made fish main shell

--- spectra/main.py
+++ spectra/main.py
@@ -1,16 +1,15 @@
 import torch
+import ollama
 
 # Generate some example data
-X = torch.randn(100, 50)  # 100 samples, 50 features
 
-# Center the data
-X_centered = X - X.mean(dim=0)
+# print(X_pca)
+model = "llama3.2"
 
-# Perform SVD
-U, S, V = torch.svd(X_centered)
+emd = ollama.embeddings(
+    model=model, 
+    prompt="Howdy how is life"
+)
 
-# Project onto the first k principal components
-k = 10
-X_pca = torch.matmul(X_centered, V[:, :k])
-
-print(X_pca)
+embedding_tensor = torch.tensor(emd["embedding"])
+print(embedding_tensor)
